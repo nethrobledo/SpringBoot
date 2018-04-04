1c3343defb9a7ddf4a351fadbb2c3674db97dd23
Neneth Robledo <neneth.robledo@riskflo.com.au>
2018-04-04 20:04:52 +1000

    Initial commit

---
+++ server/src/main/java/com/jobhunt/sample/Server.java
@@ -0,0 +1,15 @@
+package com.jobhunt.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class Server {
+    public static void main(String args[]) {
+        SpringApplication.run(Server.class, args);
+    }
+}
